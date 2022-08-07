c041b65e2206551372ba7759bc12e2e721e0c207
joohyun ha <mylifeforcoding@gmail.com>
2022-08-07 15:44:20 +0900

    setting

---
+++ src/test/java/me/hajoo/redissonstudy/RedissonStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hajoo.redissonstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
